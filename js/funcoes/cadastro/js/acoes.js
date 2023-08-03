44d23badeb25b01d8efe23b99111769877b621b8
karinefes <karinefernandes50@gmail.com>
2023-08-03 15:19:06 -0300

    cadastro

--- js/funcoes/cadastro/js/acoes.js
+++ js/funcoes/cadastro/js/acoes.js
@@ -2,3 +2,11 @@ let nome = document.getElementById('nome')
 let idade = document.getElementById('idade')
 let cargo = document.getElementById('cargo')
 let salario = document.getElementById('salario')
+
+function salvar(){
+    event.preventDefault()
+    document.getElementById('fnome').innerHTML = nome.value
+    document.getElementById('fidade').innerHTML = idade.value
+    document.getElementById('fcargo').innerHTML = cargo.value
+    document.getElementById('fsalario').innerHTML = salario.value
+}
